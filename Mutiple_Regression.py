1e69c443d1d7cfa3ae9f94f4f000ed90f1e8f8fc
Sourav Sahu <sahu.souravrb1999@gmail.com>
2021-05-07 13:15:32 +0530

    Update Mutiple_Regression.py
    Copy and Paste the SIPEstimator.xlsx File Path.

--- Mutiple_Regression.py
+++ Mutiple_Regression.py
@@ -1,33 +1,21 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[18]:
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn import linear_model
 import statsmodels.api as sm
 
+# Download the SIPEstimator.xlsx file.
+# Copy and paste the PATH in the plca of --PATH--
 
-# In[21]:
-
-
-df = pd.read_excel(r"D:\e-books\6th Sem\FML\LABS\SIPEstimator.xlsx", columns=['Year','Month','Interest_Rate','Unemployment_Rate','Stock_Index_Price'])
+df = pd.read_excel(r"--PATH--", columns=['Year','Month','Interest_Rate','Unemployment_Rate','Stock_Index_Price'])
 ir = pd.DataFrame(df, columns = ['Interest_Rate'])
 ur = pd.DataFrame(df, columns = ['Unemployment_Rate'])
 sip = pd.DataFrame(df, columns = ['Stock_Index_Price'])
 
-
-# In[17]:
-
-
 # Plotting Interest Rate vs Stock Index Price
-
-
-# In[16]:
-
 
 plt.scatter(ir, sip, color = "r", marker = "o", s = 25)
 plt.xlabel("Interest Rate")
@@ -35,13 +23,7 @@ plt.ylabel("Stock Index Price")
 plt.show()
 
 
-# In[4]:
-
-
 # Plotting Unemployment Rate vs Stock Index Price
-
-
-# In[8]:
 
 
 plt.scatter(ur, sip, color = "b", marker = "x", s = 30)
@@ -50,14 +32,8 @@ plt.ylabel("Stock Index Price")
 plt.show()
 
 
-# In[24]:
-
-
 X = df[['Interest_Rate', 'Unemployment_Rate']]
 Y = df['Stock_Index_Price']
-
-
-# In[25]:
 
 
 # with sklearn
@@ -68,37 +44,18 @@ print('Intercept: \n', regr.intercept_)
 print('Coefficients: \n', regr.coef_)
 
 
-# In[35]:
-
-
 # prediction with sklearn
 New_Interest_Rate = 2.55
 New_Unemployment_Rate = 5.4
 print ('Predicted Stock Index Price: \n', regr.predict([[New_Interest_Rate, New_Unemployment_Rate]]))
 
 
-# In[27]:
-
-
+# with statsmodels
 X = sm.add_constant(X)
-
-
-# In[28]:
-
 
 model = sm.OLS(Y, X).fit()
 predictions = model.predict(X) 
 
 
-# In[29]:
-
-
 print_model = model.summary()
 print(print_model)
-
-
-# In[ ]:
-
-
-
-
